65d0f262d92227ad7bfd12852fe5bd272104811e
Roald Ruiter <ruiter@inspire.ethz.ch>
2023-05-10 13:31:07 +0200

    add with magic methods

--- samples/pn_simple_example/setpoint_client/client.py
+++ samples/pn_simple_example/setpoint_client/client.py
@@ -73,6 +73,12 @@ class SetpointClient:
     def set_x_position(self, setpoint: int):
         self._send(set_x(setpoint))
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.disconnect()
+
     def disconnect(self):
        self.socket.close()
 
