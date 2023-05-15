a16898bcdec1bb93157b92c70b719c8e7a3ad6f7
Roald Ruiter <ruiter@inspire.ethz.ch>
2023-05-15 09:40:41 +0200

    test get_x_postition

--- samples/pn_simple_example/setpoint_client/test_client.py
+++ samples/pn_simple_example/setpoint_client/test_client.py
@@ -1,23 +1,26 @@
+import socket
 import struct
 from unittest import mock
+from unittest.mock import patch
 
-import pytest
 from assertpy import assert_that
 from client import CommandType, SetpointClient
 
-# TODO how to test this?
-@pytest.skip("TODO how to test this?")
-def test_set_x_position():
+
+@patch("socket.socket")
+def test_set_x_position(mock_socket):
     with SetpointClient("127.0.0.1", 12345) as client:
-        with mock.patch("client.socket") as mock_socket:
-            mock_socket.return_value.recv.return_value = (12, struct.pack("<B", 0x00))
-            expected_result = struct.pack(">BI", CommandType.SET_X_POSITION_UM.value, 0x00)
+        client.set_x_position(0)
 
-            expected_result = struct.pack(">BI", CommandType.SET_X_POSITION_UM.value, 0xFFFFFFFF)
-            assert_that(client.set_x.position(4294967295)).is_equal_to(expected_result)
+        mock_socket.assert_called_once_with(socket.AF_INET, socket.SOCK_DGRAM)
+        mock_socket.return_value.sendto.assert_called_once()
 
-            expected_result = struct.pack(">BI", CommandType.SET_X_POSITION_UM.value, 0x80000000)
-            assert_that(client.set_x(2147483648)).is_equal_to(expected_result)
+        expected_command = struct.pack(">BI", CommandType.SET_X_POSITION_UM.value, 0)
+        mock_socket.return_value.sendto.assert_called_with(expected_command, ("127.0.0.1", 12345))
+
+        client.set_x_position(4294967295)
+        expected_command = struct.pack(">BI", CommandType.SET_X_POSITION_UM.value, 4294967295)
+        mock_socket.return_value.sendto.assert_called_with(expected_command, ("127.0.0.1", 12345))
 
 
 def test_get_x():
